b432947921cdced1e6117aa15573a78ed65944b8
chheangdx <chheangduong@yahoo.com>
2015-01-09 22:06:02 -0800

    Added SDL_Setup class

---
+++ First-RPG/SDL_Setup.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "SDL_Setup.h"
+
+
+CSDL_Setup::CSDL_Setup(void)
+{
+}
+
+
+CSDL_Setup::~CSDL_Setup(void)
+{
+}
---
+++ First-RPG/SDL_Setup.h
@@ -0,0 +1,8 @@
+#pragma once
+class CSDL_Setup
+{
+public:
+	CSDL_Setup(void);
+	~CSDL_Setup(void);
+};
+
