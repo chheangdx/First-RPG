4f3fb40896dd24e4ed976ba3f5e939645c56266f
chheangdx <chheangduong@yahoo.com>
2015-01-09 21:20:59 -0800

    Added code for rendering grass and bob (part 6)

--- First-RPG/First-RPG.cpp
+++ First-RPG/First-RPG.cpp
@@ -20,11 +20,37 @@ int main(int argc, char* argv[])
 	SDL_Renderer* renderer = NULL;
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	SDL_Event* mainEvent = new SDL_Event();
+	SDL_Event* mainEvent = new SDL_Event(); // to get events such as pressing the close window button
+
+	SDL_Texture *grass_image = NULL;
+	grass_image = IMG_LoadTexture(renderer, "grass.bmp");
+
+	SDL_Rect grass_rect;
+	//location to start drawing
+	grass_rect.x = 0;
+	grass_rect.y = 0;
+	//width and height to draw
+	grass_rect.w = 600;
+	grass_rect.h = 500;
+
+	//do same thing but for bob image
+	SDL_Texture *bob_image = NULL;
+	bob_image = IMG_LoadTexture(renderer, "bob.png");
+
+	SDL_Rect bob_rect;
+	bob_rect.x = 300;
+	bob_rect.y = 250;
+	bob_rect.w = 80;
+	bob_rect.h = 50;
 
 	while(!quit && mainEvent->type != SDL_QUIT){
-		SDL_PollEvent(mainEvent);
+		SDL_PollEvent(mainEvent); //check for event
 		SDL_RenderClear(renderer);
+
+		SDL_RenderCopy(renderer, grass_image, NULL, &grass_rect); //put grass image with rectangle grass rect onto renderer
+		SDL_RenderCopy(renderer, bob_image, NULL, &bob_rect); //put bob image with rectangle bob rect onto renderer
+
+
 		SDL_RenderPresent(renderer);
 	}
 
